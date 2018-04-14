242f3cd853e7edf7e0fa05a67c74bbdaa9d2435d
Jacob Fischer <jacob.t.fischer@gmail.com>
2018-04-14 12:08:38 -0500

    Add pirates game

---
+++ games/pirates/index.ts
@@ -0,0 +1,27 @@
+// DO NOT MODIFY THIS FILE
+
+import { IGameNamespace } from "../../joueur/interfaces";
+import { AI } from "./ai";
+import { Game } from "./game";
+
+import { GameObject } from "./game-object";
+import { Player } from "./player";
+import { Port } from "./port";
+import { Tile } from "./tile";
+import { Unit } from "./unit";
+
+/**
+ * This is a collection of all the classes that Pirates uses to work.
+ * @hidden
+ */
+export const namespace: IGameNamespace = {
+    AI,
+    Game,
+    GameObjectClasses: {
+      GameObject,
+      Player,
+      Port,
+      Tile,
+      Unit,
+    },
+};
