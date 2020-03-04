3e24bbe9c55beb899712bc9f2bd8e37e41877d2f
Jacob Fischer <jacob.t.fischer@gmail.com>
2020-03-03 18:02:37 -0600

    Port shell ai logic from js client

--- src/games/chess/ai.ts
+++ src/games/chess/ai.ts
@@ -37,7 +37,7 @@ export class AI extends BaseAI {
     }
 
     /**
-     * This is called every time the game's state updates, so if you are tracking anything you can update it here.
+     * This is called every time the game"s state updates, so if you are tracking anything you can update it here.
      */
     public async gameUpdated(): Promise<void> {
         // pass
@@ -59,8 +59,51 @@ export class AI extends BaseAI {
      * move is invalid or not properly formatted you will lose the game.
      */
     public async makeMove(): Promise<string> {
-        // Put your game logic here for makeMove
-        return "";
+        this.printPrettyFEN();
+
+        // This will only work if we are black move the pawn at b2 to b3.
+        // Otherwise we will lose.
+        // Your job is to code SOMETHING to parse the FEN string in some way to determine a valid move, in SAN format.
+        return "b2b3";
     }
 
+    /**
+     * Pretty formats an FEN string to a human readable string.
+     *
+     * For more information on FEN (Forsyth-Edwards Notation) strings see:
+     * https://wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation
+     */
+    private printPrettyFEN() {
+        // split the FEN string up to help parse it
+        const split = this.game.fen.split(" ");
+        const first = split[0]; // the first part is always the board locations
+
+        const sideToMove = split[1]; // always the second part for side to move
+        const usOrThem = sideToMove === this.player.color[0] ? "us"  : "them";
+
+        const fullmove = split[5]; // always the sixth part for the full move
+
+        const lines = first.split("/");
+        const strings = [`Move: ${fullmove}\nSide to move: ${sideToMove} (${usOrThem})\n   +-----------------+`];
+
+        for (let i = 0; i < lines.length; i++) {
+            const line = lines[i];
+            strings.push(`\n ${8 - i} |`);
+            for (const char of line) {
+                const charAsNumber = Number(char);
+                if (isNaN(charAsNumber)) {
+                    strings.push(` ${char}`);
+                } else { // it is a number, so that many blank lines
+                    for (let j = 0; j < charAsNumber; j++) {
+                        strings.push(" .");
+                    }
+                }
+            }
+            strings.push(" |");
+        }
+        strings.push("\n   +-----------------+\n     a b c d e f g h\n");
+
+        // tslint:disable-next-line:no-console
+        console.log(strings.join(""));
+    }
 }
